531f1baf253a0bda93f60fae4c51d881781e7efd
Kanish Chhabra <kchhabra416@gmail.com>
2025-08-31 16:49:39 +0530

    project images added

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,6 +1,12 @@
 import aicoldmailerImg from "@/assets/aicoldmailer.png";
 import codecompassImg from "@/assets/codecompass.png";
 import thepodcastspaceImg from "@/assets/thepodcastspace.png";
+import portfolioImg from "@/assets/portfolio.png";
+import ridobikoImg from "@/assets/ridobiko.png";
+import aialarmImg from "@/assets/aialarm.jpg";
+import imageGeniusImg from "@/assets/imagegenius.jpg";
+import stockmateImg from "@/assets/stockmate.jpg";
+import seatingPlanImg from "@/assets/seatingplan.jpg";
 
 import type { StaticImageData } from "next/image";
 
@@ -34,47 +40,47 @@ export const Projects: Project[] = [
       "A personal portfolio website built with Next.js and Tailwind CSS.",
     localImagePathOfProject: aicoldmailerImg,
   },
-  //   {
-  //     name: "Portfolio",
-  //     projectLink: "https://kanish.in/",
-  //     description:
-  //       "A personal portfolio website built with Next.js and Tailwind CSS.",
-  //     localImagePathOfProject: "/images/portfolio.png",
-  //   },
-  //   {
-  //     name: "Ridobiko",
-  //     projectLink:
-  //       "https://play.google.com/store/apps/details?id=com.ridobikocustomer.app&pcampaignid=web_share",
-  //     description:
-  //       "A personal portfolio website built with Next.js and Tailwind CSS.",
-  //     localImagePathOfProject: "/images/portfolio.png",
-  //   },
-  //   {
-  //     name: "AI Alarm",
-  //     projectLink: "https://github.com/Kan7sh/AI.Alarm",
-  //     description:
-  //       "A personal portfolio website built with Next.js and Tailwind CSS.",
-  //     localImagePathOfProject: "/images/portfolio.png",
-  //   },
-  //   {
-  //     name: "Stock Mate Scrapper",
-  //     projectLink: "https://github.com/Kan7sh/Stock-Mate-Scrapper",
-  //     description:
-  //       "A personal portfolio website built with Next.js and Tailwind CSS.",
-  //     localImagePathOfProject: "/images/portfolio.png",
-  //   },
-  //   {
-  //     name: "Image Genius",
-  //     projectLink: "https://github.com/Kan7sh/Image-Genius",
-  //     description:
-  //       "A personal portfolio website built with Next.js and Tailwind CSS.",
-  //     localImagePathOfProject: "/images/portfolio.png",
-  //   },
-  //   {
-  //     name: "Exam Seating Plan Maker",
-  //     projectLink: "https://github.com/Kan7sh/Seating-Plan-Maker",
-  //     description:
-  //       "A personal portfolio website built with Next.js and Tailwind CSS.",
-  //     localImagePathOfProject: "/images/portfolio.png",
-  //   },
+  {
+    name: "Portfolio",
+    projectLink: "https://kanish.in/",
+    description:
+      "A personal portfolio website built with Next.js and Tailwind CSS.",
+    localImagePathOfProject: portfolioImg,
+  },
+  {
+    name: "Ridobiko",
+    projectLink:
+      "https://play.google.com/store/apps/details?id=com.ridobikocustomer.app&pcampaignid=web_share",
+    description:
+      "A personal portfolio website built with Next.js and Tailwind CSS.",
+    localImagePathOfProject: ridobikoImg,
+  },
+  {
+    name: "AI Alarm",
+    projectLink: "https://github.com/Kan7sh/AI.Alarm",
+    description:
+      "A personal portfolio website built with Next.js and Tailwind CSS.",
+    localImagePathOfProject: aialarmImg,
+  },
+  {
+    name: "Image Genius",
+    projectLink: "https://github.com/Kan7sh/Image-Genius",
+    description:
+      "A personal portfolio website built with Next.js and Tailwind CSS.",
+    localImagePathOfProject: imageGeniusImg,
+  },
+  {
+    name: "Stock Mate Scrapper",
+    projectLink: "https://github.com/Kan7sh/Stock-Mate-Scrapper",
+    description:
+      "A personal portfolio website built with Next.js and Tailwind CSS.",
+    localImagePathOfProject: stockmateImg,
+  },
+  {
+    name: "Exam Seating Plan Maker",
+    projectLink: "https://github.com/Kan7sh/Seating-Plan-Maker",
+    description:
+      "A personal portfolio website built with Next.js and Tailwind CSS.",
+    localImagePathOfProject: seatingPlanImg,
+  },
 ];
